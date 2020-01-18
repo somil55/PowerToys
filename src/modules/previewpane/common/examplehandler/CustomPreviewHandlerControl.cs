fa3b93206b17e05255575a9fa0a57e1f4e4ef6a1
Divyansh <somm14divi@gmail.com>
2020-01-17 17:18:45 -0800

    Added handler and control for Markdown

---
+++ src/modules/previewpane/common/examplehandler/CustomPreviewHandlerControl.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms;
+
+namespace Common
+{
+    /// <summary>
+    /// This is test custom control to test the implementation.
+    /// </summary>
+    public class CustomPreviewHandlerControl : FormPreviewHandlerControl
+    {
+        /// <summary>
+        /// Start the preview on the Control.
+        /// </summary>
+        /// <param name="dataSource">Path to the file.</param>
+        public override void DoPreview(IStream dataSource)
+        {
+            string filename;
+            if (dataSource is FileStream)
+            {
+                filename = ((FileStream)dataSource).Name;
+            }
+
+            base.DoPreview(dataSource);
+            this.InvokeOnControlThread(() =>
+            {
+                string filePath = ((FileStream)dataSource).Name;
+                WebBrowser browser = new WebBrowser
+                {
+                    DocumentText = "Test v7.212 " + filePath,
+                };
+                browser.Navigate(filePath);
+                browser.Dock = DockStyle.Fill;
+                browser.IsWebBrowserContextMenuEnabled = false;
+                this.Controls.Add(browser);
+                base.DoPreview(dataSource);
+            });
+        }
+    }
+}
